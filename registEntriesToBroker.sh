160ddb594070c979f3dc9fb0ed0a544753d8ea69
yugou <norio.ishizaki@gmail.com>
2015-09-01 16:09:21 +0900

    icinga to icinga_broker
    CONTAINER_NAME=icinga_broker

--- registEntriesToBroker.sh
+++ registEntriesToBroker.sh
@@ -5,22 +5,24 @@
 # to the broker running in the Icinga Docker container.
 #
 #!/bin/sh
+export CONTAINER_NAME=icinga_broker
+
 echo -n "Specify the number of entries. e.g. 1000: "
-sudo docker exec icinga /bin/bash -c "rm -f /tmp/data.txt"
+sudo docker exec $CONTAINER_NAME /bin/bash -c "rm -f /tmp/data.txt"
 read ANS
 for i in `seq 1 $ANS`
 do
     j=`expr $i + 10000`
-    sudo docker exec icinga /bin/bash -c "echo test/icinga/$i 10.10.10.1 $j >>  /tmp/data.txt"
+    sudo docker exec $CONTAINER_NAME /bin/bash -c "echo test/icinga/$i 10.10.10.1 $j >>  /tmp/data.txt"
 done
 
 start=$(date +'%s')
-sudo docker exec icinga /bin/bash -c "broklist -load localhost 39001 /tmp/data.txt"
+sudo docker exec $CONTAINER_NAME /bin/bash -c "broklist -load localhost 39001 /tmp/data.txt"
 echo "It took $(($(date +'%s') - $start)) seconds"
 
 echo
 
 echo "The number of registered entries"
 start=$(date +'%s')
-sudo docker exec icinga /bin/bash -c "broklist localhost 39001 | wc -l"
+sudo docker exec $CONTAINER_NAME /bin/bash -c "broklist localhost 39001 | wc -l"
 echo "It took $(($(date +'%s') - $start)) seconds"
