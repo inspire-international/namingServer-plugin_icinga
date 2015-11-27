a24998607af67d2adbc2cf0dc6c7b2f6d8edb666
Parshuram Patil <parshuram.patil@cogentree.com>
2015-11-27 09:59:33 +0530

    dbReset script fix
    Modified the script due to change in location of perfdata as pnp4nagios
    is installed using wget instead of apt-get

--- dbReset.sh
+++ dbReset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Initializing db"
-sudo docker exec icinga_broker rm -rf /var/lib/pnp4nagios/perfdata/
+sudo docker exec icinga_broker rm -rf /usr/local/pnp4nagios/var/perfdata
 sleep 30
 echo "Done"
